5e191903165fbae4f696f38dc0ef52fde496ba22
Alice-Retz <alicegretz@gmail.com>
2021-10-07 15:40:27 -0500

    Sign up POST route test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ describe('Lab 16 Authentication routes', () => {
     return setup(pool);
   });
 
+  it('signs up a user via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'a@a.com', password: 'pass1234' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'a@a.com',
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
